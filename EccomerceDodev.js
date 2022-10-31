f265c6949022908d7e65e15791fdd491a6836499
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-10-31 19:49:22 -0300

    Questão 3 finalizada

--- EccomerceDodev.js
+++ EccomerceDodev.js
@@ -13,5 +13,22 @@ function CadastrarProduto() {
     ids[index] =  parseInt(prompt("Insira o ID do produto:")) 
     nomes[index] = prompt ("Insira o nome do produto")
     precos[index] = parseInt(prompt("Insira o preço do produto")) 
-    avaliacoes[index] = parseInt(prompt("Insira a avaliação do produto") ) 
+    avaliacoes[index] = parseInt(prompt("Insira a avaliação do produto")) 
+    index++
+}
+
+function BuscarPorId(id) {
+    for (var contador = 0; contador < ids.length; contador++) {
+        if(id == ids[contador]){
+            console.log("O produto de ID " + id + " é o " + nome[contador] + " custa " + precos[contador] + " e tem avaliação " + avaliacoes[contador])
+        }       
+    }
+}
+
+function BuscarPorNome(nome) {
+    for (var contador = 0; contador < nomes.length; contador++) {
+        if(nome == nomes[contador]){
+            console.log("O produto de nome " + nome + " tem o ID: " + ids[contador] )
+        }
+    }
 }
