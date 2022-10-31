c6e3d36d8d7155d645b86ee0ac71e50ad31f0653
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-10-31 19:44:01 -0300

    Questão 1 finalizada

--- EccomerceDodev.js
+++ EccomerceDodev.js
@@ -2,3 +2,16 @@
 
 
 
+var ids = []
+var nomes = []
+var precos = []
+var avaliacoes = []
+var index = 0
+
+
+function CadastrarProduto() {
+    ids[index] =  parseInt(prompt("Insira o ID do produto:")) 
+    nomes[index] = prompt ("Insira o nome do produto")
+    precos[index] = parseInt(prompt("Insira o preço do produto")) 
+    avaliacoes[index] = parseInt(prompt("Insira a avaliação do produto") ) 
+}
