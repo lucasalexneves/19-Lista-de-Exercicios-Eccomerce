52e038cbacf8eea74f317f2cb4648496c72ecc44
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-10-31 20:00:35 -0300

    Questão 4 resolvida

--- EccomerceDodev.js
+++ EccomerceDodev.js
@@ -7,6 +7,12 @@ var nomes = []
 var precos = []
 var avaliacoes = []
 var index = 0
+var idsSup = []
+var nomeSup = []
+var precoSup = []
+var avaliacoesSup = []
+var indexSup = 0
+
 
 
 function CadastrarProduto() {
@@ -31,4 +37,35 @@ function BuscarPorNome(nome) {
             console.log("O produto de nome " + nome + " tem o ID: " + ids[contador] )
         }
     }
+}
+
+var maiorId = 0
+
+function OrdenarPorId() {
+    for (var contador = 0; contador < ids.length; contador++) {
+        maiorId = ids[contador]
+
+        for (var contador2 = 0; contador2 < ids.length; contador2++) {
+            if(ids[contador2] > maiorId){
+                maiorId = ids [contador2]
+            }           
+        }
+
+        for (var contador3 = 0; contador3 < ids.length; contador3++) {
+            if(maiorId == ids[contador3]){
+                idsSup[indexSup] = ids[contador3]
+                nomeSup[indexSup] = nomes[contador3]
+                precoSup[indexSup] = precos[contador3]
+                avaliacoesSup[indexSup] = avaliacoes[contador3]
+                ids[contador3] = 0
+                indexSup++
+            }            
+        }
+    }
+
+    ids = idsSup
+    nomes = nomeSup
+    precos = precoSup
+    avaliacoes = avaliacoesSup
+    indexSup = 0
 }
